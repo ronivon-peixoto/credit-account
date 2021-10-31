692cc714252f5b630f7acdee795dcee74473398e
Ronivon Peixoto Sampaio <ronivon.peixoto@gmail.com>
2021-10-31 00:24:54 -0300

    Estrutura inicial do projeto.

---
+++ src/main/java/io/pismo/creditaccount/CreditAccountApplication.java
@@ -0,0 +1,13 @@
+package io.pismo.creditaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pismo/creditaccount/CreditAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pismo.creditaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
